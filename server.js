0ea586e34b11af91bdc094a2400b21d43578edb6
nesar11 <nesar.uddin100@gmail.com>
2022-05-14 22:02:12 +0800

    register

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express=require("express");
+const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const dotenv = require("dotenv");
+// const userroutes = require("./routes/userRoutes")
+const authRouters = require("./routes/authRouters")
+const cors = require("cors");
 
 dotenv.config();
+
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(
     () => {console.log('Database is connected') },
     err => { console.log('Can not connect to the database'+ err)}
   );
 
-const app = express();
-
-app.get('/,', function(req, res) {
-    res.send('hellow from server')
-})
-
-
+app.use(cors());
 app.use(express.json());
+// app.use("/users", userroutes)
+app.use("/users", authRouters)
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
